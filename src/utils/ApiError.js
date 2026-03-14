657dffa42ca0be51b9908fe88350391de7b8e333
Misbah Pirzada <misbah.digibulls@gmail.com>
2026-03-14 12:44:07 +0500

    Seeting up user and video models

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -18,7 +18,7 @@ class ApiError extends Error{
 
         }
         else{
-            Error.captureStackTrace(this.this.constructor)
+            Error.captureStackTrace(this,this.constructor)
         }
     }
 
