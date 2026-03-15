787594ad8e8688b6224afb40f822fa632c8ee572
Misbah Pirzada <misbah.digibulls@gmail.com>
2026-03-15 05:05:10 +0500

    router and controllers setup

--- src/app.js
+++ src/app.js
@@ -16,4 +16,15 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 
 app.use(cookieParser() )
+
+
+
+//routes
+import  userRouter from './routes/user.routes.js'
+
+//routesdeclaration
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:8000/api/v1/users/registerS
+
 export{ app }
